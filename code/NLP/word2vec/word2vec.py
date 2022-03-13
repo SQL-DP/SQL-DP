7ca977436b49b88082a2a586c0e405ae256dc910
SQL-DP <101112141+SQL-DP@users.noreply.github.com>
2022-03-13 21:45:53 +0800

    Update word2vec.py

--- code/NLP/word2vec/word2vec.py
+++ code/NLP/word2vec/word2vec.py
@@ -7,4 +7,8 @@ if __name__ == '__main__':
     # train model
     sentences = LineSentence("data/SQLyuliaoku.txt")
     model = Word2Vec(sentences, vector_size=50, window=5, min_count=5, workers=4, epochs=10)
-    model.wv.save_word2vec_format("data/word2vec_50.txt", binary=False)
+    
+    model.save('model/word_embedding_50')
+    
+    # for C-MIDP, R-MIDP, and H-MIDP model
+    # model.wv.save_word2vec_format("data/word2vec_50.txt", binary=False)
